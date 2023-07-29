827aedf21545f08fee528aa3adbd7ad383c51cbe
GunPKate <gunPK>
2023-07-29 20:06:04 +0700

    random nums and try get set

--- DataStructure02/src/MyArraylistTest.java
+++ DataStructure02/src/MyArraylistTest.java
@@ -3,9 +3,12 @@ public class MyArraylistTest {
 
 	public static void main(String args[]) {
 		MyArraylist l = new MyArraylist();
-		for(int i =0; i <100; i++) {
-			l.add(i);
+		for(int i =0; i <10; i++) {
+			l.add((int) (Math.random()*100) );
 		}
+		l.set(4400, 2);
+		l.get(3);
 		System.out.println(l.toString());
+		System.out.println(l.get(3));
 	}
 }
