fc74274dabedbd3f7dd215eaff944409b0d91225
GunPKate <gunPK>
2023-07-29 20:43:09 +0700

    add particle to list

--- DataStructure02/src/GUI.java
+++ DataStructure02/src/GUI.java
@@ -2,8 +2,27 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUI extends JPanel {
+	MyArraylist list = new MyArraylist();
 	public GUI() {
-					
+		Thread t = new Thread(
+			new Runnable() {	
+				@Override
+				public void run() {
+					while(true) {
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						int n = (int)(Math.random()*50);
+						for (int i = 0; i < n; i++) {
+							list.add(n);
+						}
+					}		
+				}
+			}
+		);
+			
 		JFrame f = new JFrame();
 		f.add(this);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,9 +30,9 @@ public class GUI extends JPanel {
 		f.setVisible(true);
 	}
 	
-//	public void paint(Graphics g) {
-//		
-//	}
+	public void paint(Graphics g) {
+		
+	}
 	
 	public static void main(String args[]) {
 		new GUI();
